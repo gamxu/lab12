ca2dc56b0bbd9601d8bfe952ae7498bc7564178e
gamxu <wuttipat.did@gmail.com>
2021-12-21 15:08:20 +0700

    lab12_1 completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,28 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+
+    string grade;
+    srand(time(0));
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    int luck = rand()%8;
+
+    if(luck==0) grade = "A";
+    else if(luck==1) grade = "B+" ;
+    else if(luck==2) grade = "B";
+    else if(luck==3) grade = "C+";
+    else if(luck==4) grade = "C";
+    else if(luck==5) grade = "D+";
+    else if(luck==6) grade = "D";
+    else grade = "F";
+
+    cout << "You will get " << grade << " in this 261102.";
+
+    return 0;
+}
